13009fd80ff2413903c2566badf238c02822983a
Tony <anthonyhch12@gmail.com>
2023-01-06 06:33:42 -0600

    The first changes were finalized

---
+++ main.py
@@ -0,0 +1,10 @@
+import modulo
+
+def mostrar():
+    print(f"Esta es una suma {modulo.suma(2,3)}")
+    print(f"Esta es una resta {modulo.resta(2,3)}")
+    print(f"Esta es una multi {modulo.multi(2,3)}")
+    print(f"Esta es una div {modulo.div(2,3)}")
+
+
+mostrar()
---
+++ modulo.py
@@ -0,0 +1,13 @@
+
+
+def suma(num1 , num2):
+    return num1 + num2
+
+def resta(num1 , num2):
+    return num1 - num2
+
+def multi(num1,num2):
+    return num1 * num2
+
+def div(num1,num2):
+    return num1 / num2
